4f5836693e7cf2c6cfb6c85c652e5704d3cfcd43
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-28 19:35:44 +0300

    Style close buttohn

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,20 @@
 const nav = document.querySelector("nav");
 const button = document.querySelector(".close-nav-btn");
 
+
+
 button.addEventListener('click', () => {
     if (nav.classList.contains("nav-mobile-closed"))
     {
+        console.log("if section");
         nav.classList.remove("nav-mobile-closed");
         button.classList.remove("fixed");
     }
     else
     {
+        console.log("else section");
         nav.classList.add("nav-mobile-closed");
         button.classList.add("fixed");
     }
 });
+
